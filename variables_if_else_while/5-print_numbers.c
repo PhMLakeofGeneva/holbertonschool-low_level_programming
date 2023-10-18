7bd7f0348acb0f90419395a6e0a4994c7bb973d1
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-18 00:51:35 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *  main - main
+ *   Description: Print all possible combinations of single-digit numbers.
+ *   Numbers must be separated by commas and a space.
+ *   Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x=48; x < 58; x++)
+	{
+		putchar(x);
+		if (x < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
