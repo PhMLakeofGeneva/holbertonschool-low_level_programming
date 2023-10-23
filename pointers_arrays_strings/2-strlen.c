ea9c525ee1d194843a074e7a53326575734e3512
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-23 06:41:34 -0700

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-* int_strlen - this function returns the length of s string
+* _strlen - this function returns the length of s string
 * @s : input string
 * Return: length of a string
 */
 int _strlen(char *s)
 {
 	int count = 0;
+
 	while (*(s + count) != '\0')
-	count++;
+		count++;
 	return (count);
 }
