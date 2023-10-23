91b4d06126517013848f1d2c0efb5c0548af0434
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-23 02:43:10 -0700

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it point to 98.
- * 
- * @n: An integer input printer
- *
- * Return: nothing 
+ * reset_to_98 - takes a pointer to an int as parameter
+ * @n: An integer input printer *
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {
