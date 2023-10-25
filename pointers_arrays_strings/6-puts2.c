73b0317995407ac3b4e8795ee027b699854f06c8
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-25 01:38:40 -0700

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,24 @@
-#include <main.h>
+#include "main.h"
 /**
- *  main - rints every other character of a string, starting with the first character, followed by a new line.
- *  Returne: Always 0.
- */
-
-int main(void)
+  * puts2 - Prints every other character of a string
+  * @str: The string to be treated
+  *
+  * Return: void
+  */
+void puts2(char *str)
 {
-	char *str;
+	int i;
+	int j = 0;
 
-	str = "0123456789";
-	puts2(str);
-	return (0);
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
