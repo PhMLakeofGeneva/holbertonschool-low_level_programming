5dcb16a4f7b6453446043931f565fb94908375dd
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-25 01:56:58 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string
+ * @str: The string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
