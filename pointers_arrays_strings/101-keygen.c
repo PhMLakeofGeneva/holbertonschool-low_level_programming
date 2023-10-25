de2e4d8860f9998a67b46f2038bfc453b5dbc72f
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-25 07:12:53 -0700

    101-keygen.c

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -5,11 +5,11 @@
 int main(void)
 {
 	int myrand,
-	    int count;
+	int count;
 	int total;
 
 	srand(time(NULL));
-	for 'count = 0, total = 2772; total > 122; count++)
+	for (count = 0; total = 2772; total > 122; count++)
 	{
 		myrand = (rand() % 125) + 1;
 		printf("%c", myrand);
