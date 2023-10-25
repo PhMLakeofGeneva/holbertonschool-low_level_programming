108cc14c4038463609c5d908b0698caed3ad8a9d
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-25 01:30:57 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  main - rints every other character of a string, starting with the first character, followed by a new line.
+ *  Returne: Always 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
