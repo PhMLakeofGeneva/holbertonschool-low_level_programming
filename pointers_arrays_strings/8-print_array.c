004caa60751031d181fd4097a48fcf516b7bae4f
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-25 02:49:29 -0700

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 # include <stdio.h>
 
 /**
- * print_array - Print n elements of an array of integers, followed by anew line.
+ * print_array - Print n elements of an array of integers.
  * @a: Array of integers
  * @n: Number of elements of the array to be printed
  *
