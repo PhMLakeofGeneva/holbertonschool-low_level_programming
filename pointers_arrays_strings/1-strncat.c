36199407b7a0bda1de351805cdc5cec2dd52b956
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-26 05:26:49 -0700

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,28 @@
 #include "main.h"
-
 /**
- * _strncat - function that concatenates two strings. it will use
+ *_strncat - function that concatenates two strings. it will use
  * at most n bytes from src. src does not need to be null terminated.
- * @dest: pointer to destination string.
- * @src: pointer to source string.
- * @n: number of bytes to be concatenated.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
  *
  *Return: pointer to destination string.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int a, b;
-	for (a = 0; dest[a] != '\0'; a++);
+	int length, j;
+/* j is a counter for  n bytes of src to be concatenated */
+/* length = length of destination string */
 
-	for (b = 0; b < n && src[b] != '\0'; b++)
+	length = 0;
+	while (dest[length] != '\0')
 	{
-		dest[a + b] = src[b];
+		length++;
 	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
 	return (dest);
 }
