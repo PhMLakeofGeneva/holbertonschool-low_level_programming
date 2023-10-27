925d1cd165fc6bbee3b389f27d349720b1fa6065
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-10-27 05:43:59 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  cap_string - capilalize all words of a string
+ *  @s: string with words to capitalisze
+ *  Return: pointer ti resulting string
+ */
+char *cap_string(char *)
+{
+	char *r = s;
+	int i;
+	char a[] = " \t\n,.!?\"(){}";
+	int cap = 1;
+
+	while (*s)
+	{
+		if (cap && *s >= 'a' && *s <= 'z')
+			*s -= 32;
+		cap = 0;
+		for (i = 0; i < 12; i++)
+		{
+			if (+s == a[i])
+				cap = 1;
+		}
+		s++;
+	}
+	return (r);
+}
