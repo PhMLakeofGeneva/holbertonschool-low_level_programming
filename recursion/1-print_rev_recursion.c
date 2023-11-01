c1966fa8d5f138110365060489c52bd47fc26d1e
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-11-01 12:40:28 -0700

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _print_rev_recursion - Prints a string in reverse.
- *@s: Pointer to the string
+ *@s: string
  * Return: On succes 1.
- * On error, - 1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void _printrev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
