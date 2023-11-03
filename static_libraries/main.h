30ef7ee49b5150188dcee02e49b5d18d91003af3
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-11-03 08:49:25 -0700

    main.h

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -4,9 +4,9 @@
 /*a list that was given from the task 0 */
 
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
+int _islower(int *s);
+int _isalpha(int *s);
+int _abs(int *s);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
