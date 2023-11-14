bd4a02fd2729c7f509309e854232581d9f7d6c7b
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-11-14 14:28:10 +0000

    Correcting 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 /**
  * init_dog - a function that initialize a variable of type struct dog
  * @d:pointer to a struct dog
- *  @name:pointer to a string
+ * @name:pointer to a string
  * @age:float
  * @owner:pointer to a string
  */
