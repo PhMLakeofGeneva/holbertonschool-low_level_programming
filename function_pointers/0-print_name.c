5f2adbd32a9fc37f54ab8180cd1f99cb076e674f
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-11-15 15:31:53 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - Prints a name
-  * @name: The name to prints
-  * @f: Pointer to function
-  *
-  * Return: Nothing
-  */
+ * print_name - prints a name.
+ * @name: string to print.
+ * @f: fuction to use.
+ *
+ * Return: NOTHING
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (!f || !name)
+		return;
+	f(name);
 }
