1192a5436101023d4c3ad69a4570e20556d814ac
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-12-07 15:09:47 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,38 +1,38 @@
-#include <std>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - function that creates a file.
- * @filemane: variable pointer
- * @text_content: content file
- * Description: Create a function that creates a file.
- * Return: 1 on success, -1 on failure
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-    int i = 0, file;
+	int fd;
+	int nletters;
+	int rwr;
 
-    if (filename == NULL)
-            return (-1);
+	if (!filename)
+		return (-1);
 
-    if (text_content == NULL)
-            text_content = "";
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    while (text_content[i] != '\0')
-    {
-        i++;
+	if (fd == -1)
+		return (-1);
 
-    }
+	if (!text_content)
+		text_content = "";
 
-    file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-    if (file == -1);
-            return (-1);
+	rwr = write(fd, text_content, nletters);
 
-    write(file, text_content, i);
-    
-    return (1);
-    
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
