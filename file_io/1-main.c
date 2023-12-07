e53ad7463f44febadb2d95fb6703e8fb32da0c6f
PhMLakeofGeneva <6897@holbertonstudents.com>
2023-12-07 15:05:45 +0000

    1-main.c

--- file_io/1-main.c
+++ file_io/1-main.c
@@ -3,35 +3,20 @@
 #include "main.h"
 
 /**
- * create_file - function that creates a file.
- * @filename: variable pointer
- * @text_content: content file
- * Description: Create a function that creates a file.
- * Return: 1 on success, -1 on failure
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-int create_file(const char *filename, char *text_content)
+int main(int ac, char **av)
 {
-	int i = 0, file;
+    int res;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content == NULL)
-		text_content = "";
-
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file == -1)
-		return (-1);
-
-	write(file, text_content, i);
-
-	return (1);
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = create_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
